54fb52212c299cabdf6d39bd791be1de61991454
Swaathi K <swaathi@skcript.com>
2017-01-06 12:27:38 +0530

    Converting to classification + Adding accuracy

--- grains/__init__.py
+++ grains/__init__.py
@@ -3,14 +3,20 @@ from brain import Brain
 from dirpath import DIRPATH
 
 GRAINS = [
-            {"group": "chick-peas", "group_id": 1, "path": DIRPATH + "/data/chick-peas/"},
-            {"group": "green-peas", "group_id": 2, "path": DIRPATH + "/data/green-peas/"},
-            {"group": "rice", "group_id": 3, "path": DIRPATH + "/data/rice/"}
+            {"group": "chick-peas", "group_id": 0, "path": DIRPATH + "/data/chick-peas/"},
+            {"group": "green-peas", "group_id": 1, "path": DIRPATH + "/data/green-peas/"},
+            {"group": "rice", "group_id": 2, "path": DIRPATH + "/data/rice/"}
         ]
 
 class Grain():
     def __init__(self):
         self.brain = Brain()
+
+    def save(self, location):
+        self.brain.save(file_name=location)
+
+    def load(self, location):
+        self.brain.load(file_name=location)
 
     def train(self):
         for grain in GRAINS:
